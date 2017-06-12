63f59d19157660846edba494092713076fb2eade
eduardosarreta <eduardosarreta@gmail.com>
2017-06-12 10:25:28 -0500

    with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -83,13 +83,6 @@ ActiveRecord::Schema.define(version: 20170611232255) do
     t.datetime "updated_at",          null: false
   end
 
-  create_table "red_flags_expenses_deputies", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "deputy_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "senators", force: :cascade do |t|
     t.string   "name"
     t.string   "party"
